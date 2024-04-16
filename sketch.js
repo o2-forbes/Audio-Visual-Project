c89ed591fcac4403d00a1622d89c1e9cf9f6a249
o2-forbes <oliver2.forbes@live.uwe.ac.uk>
2024-04-16 18:48:58 +0100

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -7,20 +7,26 @@ var drop = []; // Array function that holds drops
 var stars = []; // Array function that holds stars
 var song1; // Declare a variable named 'song' to be used for storing a reference to a sound file
 var song2; // Declare a variable named 'song' to be used for storing a reference to a sound file
+var slider;
 
 // Preload function to load the sound file before the sketch starts
 function preload() {
   // Load the sound files and assign it to the appropriate 'song' variable
-  song1 = loadSound("Rainy Village Royalty Free 8 Bit Lofi Hip Hop.mp3");
-  song2 = loadSound("Arukas Bloom Royalty Free 8 Bit Lofi Hip Hop.mp3");
+  song1 = loadSound("Arukas Bloom Royalty Free 8 Bit Lofi Hip Hop.mp3");
+  song2 = loadSound("Rainy Village Royalty Free 8 Bit Lofi Hip Hop.mp3");
 }
 
 function setup() {
   // Create a canvas with width 800 pixels and height 900 pixels
   createCanvas(800, 900);
+  // Create a slider element with the following parameters:
+  // - Minimum value: 0
+  // - Maximum value: 1
+  // - Initial value: 0.5
+  // - Step size: 0.01
+  slider = createSlider(0, 1, 0.5, 0.01);
   // Play the loaded sound
-  song2.play();
-  song2.setVolume(0.3);
+  song1.play();
 
   // Stars
   for (var s = 0; s < 1000; s++) {
@@ -116,6 +122,9 @@ function makeCloud(cloudx, cloudy) {
 }
 
 function draw() {
+  // Set the volume of song1 to the current value of a slider element
+  song1.setVolume(slider.value());
+
   if (on) {
     // Define the background gradient
     let backgroundGradient = drawingContext.createLinearGradient(
